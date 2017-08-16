a519808bfdd603ebe817d28faaa1baad14a5815c
Matheus Barone <matheusvbarone@gmail.com>
2017-08-16 13:30:09 -0300

    Comentando o codigo JS

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
+//VARIÁVEL DOS INPUTS
 const inputs = document.querySelectorAll('.controls input');
 
+
+//FUNÇÃO QUE MUDA A VARIAVEL CSS
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
+
+//CALLBACK
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
